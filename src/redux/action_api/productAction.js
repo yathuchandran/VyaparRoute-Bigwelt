f10276bdbd33d46722d549a644e8ec8cb66f025e
Mohammed moin <moinmisba92@gmail.com>
2024-09-05 13:16:10 +0530

    product update

--- src/redux/action_api/productAction.js
+++ src/redux/action_api/productAction.js
@@ -2,9 +2,18 @@ import {
   ADD_STAFF_FAILURE,
   ADD_STAFF_REQUEST,
   ADD_STAFF_SUCCSESS,
+  ALL_CUSTOMER_FAILURE,
+  ALL_CUSTOMER_REQUEST,
+  ALL_CUSTOMER_SUCCSESS,
   CREATE_PRODUCT_FAILURE,
   CREATE_PRODUCT_REQUEST,
   CREATE_PRODUCT_SUCCSESS,
+  FETCH_FAILURE,
+  FETCH_REQUEST,
+  FETCH_SUCCSESS,
+  STAFF_GROUP_FAILURE,
+  STAFF_GROUP_REQUEST,
+  STAFF_GROUP_SUCCSESS,
 } from "../constant/productConstant";
 import axios from "axios";
 
@@ -58,13 +67,108 @@ export const AddStaffAction = (formdata) => async (dispatch) => {
       }
     );
 
+    console.log(data);
+
     dispatch({
       type: ADD_STAFF_SUCCSESS,
       payload: data,
     });
   } catch (error) {
+    console.log(error);
+
     dispatch({
       type: ADD_STAFF_FAILURE,
+      payload: error.response?.data?.message || error.message,
+    });
+  }
+};
+
+export const GetAllGroup = () => async (dispatch) => {
+  try {
+    dispatch({ type: FETCH_REQUEST });
+
+    const { data } = await axios.get(
+      "https://vr.w4u.in/manage/api/customers_group/all",
+
+      {
+        headers: {
+          "X-Api-Key": "8YUI3673DEB6F281A8F2E856902HJKU7",
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    dispatch({
+      type: FETCH_SUCCSESS,
+      payload: data.data,
+    });
+  } catch (error) {
+    console.log(error);
+
+    dispatch({
+      type: FETCH_FAILURE,
+      payload: error.response?.data?.message || error.message,
+    });
+  }
+};
+
+export const addStaffGroup = (formdata, selectedGroups) => async (dispatch) => {
+  try {
+    dispatch({ type: STAFF_GROUP_REQUEST });
+
+    // Convert the array to a comma-separated string
+    const group_name = selectedGroups.join(", ");
+
+    console.log(group_name);
+
+    const group = {
+      group_name, // group_name is now a string
+      staff_name: formdata.staff_name,
+      staff_mobile: formdata.staff_mobile,
+    };
+
+    const { data } = await axios.post(
+      "https://vr.w4u.in/manage/api/customers_group/add",
+      group,
+      {
+        headers: {
+          "X-Api-Key": "8YUI3673DEB6F281A8F2E856902HJKU7",
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    dispatch({
+      type: STAFF_GROUP_SUCCSESS,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
+
+    dispatch({
+      type: STAFF_GROUP_FAILURE,
+      payload: error.response?.data?.message || error.message,
+    });
+  }
+};
+
+export const allCustomers = () => async (dispatch) => {
+  try {
+    dispatch({ type: ALL_CUSTOMER_REQUEST });
+
+    const { data } = await axios.get("https://vr.w4u.in/manage/api/users/all", {
+      headers: {
+        "X-Api-Key": "8YUI3673DEB6F281A8F2E856902HJKU7",
+      },
+    });
+
+    dispatch({
+      type: ALL_CUSTOMER_SUCCSESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_CUSTOMER_FAILURE,
       payload: error.response?.data?.message || error.message,
     });
   }
--- src/redux/reducer/productReducer.js
+++ src/redux/reducer/productReducer.js
@@ -2,9 +2,18 @@ import {
   ADD_STAFF_FAILURE,
   ADD_STAFF_REQUEST,
   ADD_STAFF_SUCCSESS,
+  ALL_CUSTOMER_FAILURE,
+  ALL_CUSTOMER_REQUEST,
+  ALL_CUSTOMER_SUCCSESS,
   CREATE_PRODUCT_FAILURE,
   CREATE_PRODUCT_REQUEST,
   CREATE_PRODUCT_SUCCSESS,
+  FETCH_FAILURE,
+  FETCH_REQUEST,
+  FETCH_SUCCSESS,
+  STAFF_GROUP_FAILURE,
+  STAFF_GROUP_REQUEST,
+  STAFF_GROUP_SUCCSESS,
 } from "../constant/productConstant";
 
 // const initialState = {
@@ -42,14 +51,16 @@ export const PrdReducer = (state = { product: [] }, action) => {
 };
 
 export const AddStaf = (state = {}, action) => {
-  switch (action) {
+  switch (action.type) {
     case ADD_STAFF_REQUEST:
+    case STAFF_GROUP_REQUEST:
       return {
         loading: true,
         ...state,
       };
 
     case ADD_STAFF_SUCCSESS:
+    case STAFF_GROUP_SUCCSESS:
       return {
         loading: false,
         ...state,
@@ -57,9 +68,45 @@ export const AddStaf = (state = {}, action) => {
       };
 
     case ADD_STAFF_FAILURE:
+    case STAFF_GROUP_FAILURE:
       return {
         loading: false,
         error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const FetchAll = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_REQUEST:
+    case ALL_CUSTOMER_REQUEST:
+      return {
+        loading: true,
+        ...state,
+      };
+
+    case FETCH_SUCCSESS:
+      return {
+        loading: false,
+        ...state,
+        Allgroup: action.payload,
+      };
+
+    case ALL_CUSTOMER_SUCCSESS:
+      return {
+        loading: false,
+        ...state,
+        allCustomers: action.payload,
+      };
+
+    case FETCH_FAILURE:
+    case ALL_CUSTOMER_FAILURE:
+      return {
+        loading: false,
+        error: action.payload,
+        ...state,
       };
     default:
       return state;
